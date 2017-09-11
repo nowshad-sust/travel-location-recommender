fb4b88fa5441ddd5d228d948286367c379522d51
nowshad-sust <iamnowshad@gmail.com>
2017-09-12 01:19:22 +0600

    removed comments from main to readme

--- main.go
+++ main.go
@@ -11,34 +11,6 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-/**
-this is a location recommendation system
-this system will suggest user where should
-they belong based on their feedback.
-
-# every question should have multiple choices
-# each option will consist some text or image
-# eatch option should value some tags
-# tag subjects: weather,
-				food,
-				culture,
-				men/women,
-				places/travel,
-				jobs/occupation,
-
-# based on the tags, find the most closet matched
-	location/place implementing some basic machine
-	learning techniques.
-
-# questions can be static, and all of the form should
-	be loaded in the front end without creating any
-	backend load. Just the final tags array should be
-	posted to the back end and wait for response.
-
-# sleek UI/UX must be implemented for of all the front end
-	effects and functionalities.
-
-*/
 var JSON []byte
 
 func getData() {
